90f1a5dbfabe861040e01f01fc07d57a69332572
zhengqinhong <369309988@qq.com>
2015-10-05 13:59:37 +0800

    初始化

---
+++ CircleimageLoder/CircleimageLoder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircleimageLoder
+//
+//  Created by 郑钦洪 on 15/10/5.
+//  Copyright (c) 2015年 Kingsly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
